de40882b1708fcc61287b0a4a2f88734c7840708
Mai <ttmai@my.loyno.edu>
2024-07-22 19:28:57 -0400

    add checkbox

---
+++ dom_manipulation.js
@@ -0,0 +1,28 @@
+// First up, let's create a new list item and store it in a variable.
+var newListItem = document.createElement("li");
+
+// Now let's update the text content of that list item.
+newListItem.textContent = "Jalapenos";
+
+// And finally, let's add that list item as a child of the ul.
+document.querySelector("ul").appendChild(newListItem);
+
+const myDiv = document.getElementById("myDiv");
+
+const itemIngredients = myDiv.querySelectorAll("ul li");
+
+for (let i = 0; i < itemIngredients.length; i++){
+    let checkbox = document.createElement("input");
+
+    var label  = document.createElement("label"); 
+    
+    label.htmlFor = "id"; 
+    
+    checkbox.type = "checkbox"; 
+    checkbox.name = "name"; 
+    checkbox.value = "value"; 
+    checkbox.id = "id"; 
+
+    itemIngredients[i].appendChild(checkbox);
+    itemIngredients[i].appendChild(label);
+}
