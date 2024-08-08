f5a3844bdf841965b053ae206dcef999f89a72a9
Mai <ttmai@my.loyno.edu>
2024-08-08 13:58:18 -0400

    use dom for HTML

--- mai_Game /mai_Game.js
+++ mai_Game /mai_Game.js
@@ -4,8 +4,19 @@
 // create a title for the page
 //Body html 
     //create a header that's Mai Game
-        // it's a user input for Earth, Wind, Fire
 
+    //documentation to create Element : https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+       
+const maiGameHeader = document.createElement("h1");
+//create the content for the h1
+const h1Content = document.createTextNode("Earth, Wind, Fire: Mai's Take on the 'Rock, Paper, Scissors' Game");//Append the h1Content to "h1"
+//Append h1 to the maiGameHeader 
+maiGameHeader.appendChild(h1Content); 
+
+//Append maiGameHeader to the body 
+document.body.appendChild(maiGameHeader);
+
+     // it's a user input for Earth, Wind, Fire
     // this is a basic rock, paper, scissor game 
     // there is a user.input 
     // there is a randomize computer that will insert a num(0,1,3)
