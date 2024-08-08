ecca21412b9ce8a887d2d756323288421935d60c
Mai <ttmai@my.loyno.edu>
2024-08-08 13:36:41 -0400

    psuedo-code for Earth, Wind, & Fire Game

---
+++ mai_Game /mai_Game.js
@@ -0,0 +1,19 @@
+//Psuedo-code for Earth, Wind & Fire: Mai's take on Rock, Paper, Scissor
+
+// create an html template 
+// create a title for the page
+//Body html 
+    //create a header that's Mai Game
+        // it's a user input for Earth, Wind, Fire
+
+    // this is a basic rock, paper, scissor game 
+    // there is a user.input 
+    // there is a randomize computer that will insert a num(0,1,3)
+
+// a bunch of if statements for the rules of the game 
+    // Earth takes out Fire 
+    // Wind blows away Earth 
+    // Fire consumes Wind/Air 
+
+    
+
