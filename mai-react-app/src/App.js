7d1c40e1115ab527e5b1f282ace8c308c2098dd7
Mai <ttmai@my.loyno.edu>
2024-08-14 22:24:10 -0700

    Game logic

--- mai-react-app/src/App.js
+++ mai-react-app/src/App.js
@@ -6,9 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <GameLogic 
-        title={"Earth, Wind & Fire: Mai's Take on the 'Rock, Paper, Scissors' Game"} 
-        description={"Hi, welcome to Mai take on 'Rock, Paper, Scissors'. Here are the *RULES*. Earth takes out Fire. Wind blows away Earth. Fire consumes Wind. Have fun!"}/>
+      <GameLogic/>
       </header>
     </div>
   );
