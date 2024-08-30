6e55ecec2f77779f2bd08b3919c683682f427b33
Mai <ttmai@my.loyno.edu>
2024-08-30 15:00:44 -0400

    commiting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'; 
 import { useState } from 'react';
-import Icon from "/.Icon.jsx"
+import Icon from "./Icon.jsx"
 
 //documentation: https://www.youtube.com/watch?v=_Hhg7NmmN-c
 
@@ -28,7 +28,7 @@ function App() {
       <header className='App-header'>
         <h1>Weather App</h1>
         <div> 
-          
+        <Icon iconCode={weatherData.weather[0].icon} />
           <input
             type="text"
             placeholder="Ex. Vienna, Budapest, Hasselt"
@@ -41,7 +41,6 @@ function App() {
         
         {weatherData && (
           <div className="weather-info">
-            <Icon iconCode={weatherData.weather[0].icon} />
             <p>Location: {weatherData.name}</p>
             <p>Temperature: {weatherData.main.temp} °F</p>
             <p>Condition: {weatherData.weather[0].description}</p>
