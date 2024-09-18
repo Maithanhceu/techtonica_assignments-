45e533fc8bf45ca14e950d55f98e113eb4972a67
Mai <60695595+Maithanhceu@users.noreply.github.com>
2024-09-18 11:00:38 -0500

    Revert "Issues with testing"
    This reverts commit 87ae7cb7b133e6e30aaf988c825b08dd2a8a1778.

--- mai_eventechtonica/client/src/App.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
-import MyForm from './components/Form';
-
-describe('MyForm', () => {
-  test('renders MyForm Component', () => {
-      render(<MyForm />);
-  });
-});
