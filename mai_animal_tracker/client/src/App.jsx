d42e8c5f0851c4fdc7a8c2977dd6665082d2b87d
Mai <ttmai@my.loyno.edu>
2024-09-19 11:48:39 -0500

    setting up the front and the backend for the APP

---
+++ mai_animal_tracker/client/src/App.jsx
@@ -0,0 +1,13 @@
+// import {useState, useEffect} from 'react'; 
+//rfce (will create a funcational component for App 
+function App() {
+  // const [message, setMessage] = 
+
+  return (
+    <div className='Mai App'>
+      <h1>Mai  Animal Tracking App</h1>
+    </div>
+  )
+}
+
+export default App
