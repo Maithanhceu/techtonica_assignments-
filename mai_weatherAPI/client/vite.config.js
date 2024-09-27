02d0414042982f4a98b80c1af30c53a031eda77a
Mai <ttmai@my.loyno.edu>
2024-09-27 14:02:43 -0400

    used another api to autopopulate location:

--- mai_weatherAPI/client/vite.config.js
+++ mai_weatherAPI/client/vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/weather': 'http://localhost:5000',
-    }
-  }
+      '/weather': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
+    },
+  },
 });
+
